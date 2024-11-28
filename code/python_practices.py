dc4679b0412d5c6cb0b14b6206eba8b69fafb52e
SSSanga <ksa901125@gmail.com>
2024-11-28 23:10:36 +0900

    백준_단계별_사칙연산_END

--- code/python_practices.py
+++ code/python_practices.py
@@ -1,3 +1,17 @@
+#1008번
+A, B= map(int, input().split())
+
+print(A/B)
+
+#1000번
+# A, B= map(int, input().split())
+
+# print(A+B)
+
+
+# or
+# A, B, C = [int(x) for x in input().split()]
 # print("\    /\ \n )  ( ')\n(  /  )\n \(__)|")
 
-print('|\_/| \n|q p|   /} \n( 0 )"""\ \n|"^"`    | \n||_/=\\\__|')
+#print('|\_/| \n|q p|   /} \n( 0 )"""\ \n|"^"`    | \n||_/=\\\__|')
+
