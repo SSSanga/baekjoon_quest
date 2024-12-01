75b9e4f26e6d522f2f19ac7fc58f28f894f3d142
SSSanga <ksa901125@gmail.com>
2024-12-01 23:10:53 +0900

    what change?

--- code/python_practices.py
+++ code/python_practices.py
@@ -1,19 +1,23 @@
+score = int(input())
+
+if 90 <= score <= 100:
+    print('A')
+elif 80 <= score < 90:
+    print('B')
+elif 70 <= score < 80:
+    print('C')
+elif 60 <= score < 70:
+    print('D')
+else:
+    print('F')
+
+
 #1330번
 # 첫째 줄에 다음 세 가지 중 하나를 출력한다.
 
 # A가 B보다 큰 경우에는 '>'를 출력한다.
 # A가 B보다 작은 경우에는 '<'를 출력한다.
 # A와 B가 같은 경우에는 '=='를 출력한다.
-
-A, B = map(int, input().split())
-
-if A > B :
-    print('>')
-elif A < B :
-    print('<')
-elif A == B :
-    print('==')
-
 #1008번
 # A, B= map(int, input().split())
 
