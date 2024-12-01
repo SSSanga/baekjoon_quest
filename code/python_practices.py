87bba25c336e1e0b2ef78ddf911016736ed87d82
SSSanga <ksa901125@gmail.com>
2024-12-01 22:44:43 +0900

    1330번

--- code/python_practices.py
+++ code/python_practices.py
@@ -1,7 +1,23 @@
-#1008번
-A, B= map(int, input().split())
+#1330번
+# 첫째 줄에 다음 세 가지 중 하나를 출력한다.
 
-print(A/B)
+# A가 B보다 큰 경우에는 '>'를 출력한다.
+# A가 B보다 작은 경우에는 '<'를 출력한다.
+# A와 B가 같은 경우에는 '=='를 출력한다.
+
+A, B = map(int, input().split())
+
+if A > B :
+    print('>')
+elif A < B :
+    print('<')
+elif A == B :
+    print('==')
+
+#1008번
+# A, B= map(int, input().split())
+
+# print(A/B)
 
 #1000번
 # A, B= map(int, input().split())
